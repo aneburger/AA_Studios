972ecfa285964c82745bf0dd158667ae1a04695d
Anica-Ferreira <anicaferr@gmail.com>
2026-09-03 23:02:43 +0200

    Fix music lag issue, add mutation card images

--- MarshRooms!/Assets/Scripts/Boons/BoonSelectionUI.cs
+++ MarshRooms!/Assets/Scripts/Boons/BoonSelectionUI.cs
@@ -8,8 +8,14 @@ public class BoonSelectionUI : MonoBehaviour
 
     [SerializeField] private GameObject panelRoot;
     [SerializeField] private BoonCardSlotUI[] cardSlots;
+    [SerializeField] private UnityEngine.UI.Button skipButton;
+
+    [Header("Audio")]
+    [SerializeField] private AudioClip skipSfx;
+    [Range(0f, 1f)] [SerializeField] private float skipSfxVolume = 0.6f;
 
     private Action<BoonCardData> currentCallback;
+    private Action currentSkipCallback;
 
     // -- AWAKE --
     private void Awake()
@@ -19,14 +25,23 @@ public class BoonSelectionUI : MonoBehaviour
 
         if (panelRoot != null)
             panelRoot.SetActive(false);
+
+        if (skipButton != null)
+            skipButton.onClick.AddListener(HandleSkip);
     }
 
     // -- SHOW --
-    public void Show(List<BoonCardData> offers, Action<BoonCardData> onPicked)
+    public void Show(List<BoonCardData> offers, Action<BoonCardData> onPicked, Action onSkipped = null)
     {
         currentCallback = onPicked;
+        currentSkipCallback = onSkipped;
+
         panelRoot.SetActive(true);
         FindPlayerShooter()?.SetCanShoot(false);
+        Time.timeScale = 0f;
+
+        if (skipButton != null)
+            skipButton.gameObject.SetActive(onSkipped != null);
 
         for (int i = 0; i < cardSlots.Length; i++)
         {
@@ -47,8 +62,24 @@ public class BoonSelectionUI : MonoBehaviour
     {
         panelRoot.SetActive(false);
         FindPlayerShooter()?.SetCanShoot(true);
+        Time.timeScale = 1f;
         currentCallback?.Invoke(chosen);
         currentCallback = null;
+        currentSkipCallback = null;
+    }
+
+    // -- HANDLE SKIP --
+    private void HandleSkip()
+    {
+        if (skipSfx != null)
+            AudioManager.Instance.PlaySFX(skipSfx, skipSfxVolume);
+
+        panelRoot.SetActive(false);
+        FindPlayerShooter()?.SetCanShoot(true);
+        Time.timeScale = 1f;
+        currentSkipCallback?.Invoke();
+        currentCallback = null;
+        currentSkipCallback = null;
     }
 
     // -- FIND SHOOTER --
--- MarshRooms!/Assets/Scripts/RoomsScenes/Elevator.cs
+++ MarshRooms!/Assets/Scripts/RoomsScenes/Elevator.cs
@@ -59,16 +59,19 @@ public class Elevator : MonoBehaviour
 
         interactable.SetInteractionLocked(true);
         AudioManager.Instance.CrossfadeMusic(elevatorTuneClip, 0.5f, elevatorTuneClipVolume);
-        Time.timeScale = 0f;
 
         int floorNumber = LevelLoader.Instance.GetCurrentFloorNumber();
         List<BoonCardData> offers = BoonManager.Instance.GetThreeCardOffers(floorNumber);
 
-        BoonSelectionUI.Instance.Show(offers, chosenCard =>
-        {
-            BoonManager.Instance.ApplyBoonById(chosenCard.boonId);
-            ProceedToNextFloor();
-        });
+        BoonSelectionUI.Instance.Show(
+            offers,
+            chosenCard =>
+            {
+                BoonManager.Instance.ApplyBoonById(chosenCard.boonId);
+                ProceedToNextFloor();
+            },
+            onSkipped: ProceedToNextFloor
+        );
     }
 
     private void ProceedToNextFloor()
@@ -77,6 +80,5 @@ public class Elevator : MonoBehaviour
 
         string nextScene = LevelLoader.Instance.GetNextFloorSceneName();
         LevelLoader.Instance.LoadLevel(nextScene);
-        Time.timeScale = 1f;
     }
 }
