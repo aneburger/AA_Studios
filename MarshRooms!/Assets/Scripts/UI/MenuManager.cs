774d132623550450aae0edeb99fd0a8d9a911e9c
Anica-Ferreira <anicaferr@gmail.com>
2026-09-12 18:57:31 +0200

    New floor 03

--- MarshRooms!/Assets/Scripts/UI/MenuManager.cs
+++ MarshRooms!/Assets/Scripts/UI/MenuManager.cs
@@ -20,6 +20,9 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private QuitConfirmManager quitConfirmManager;
     [SerializeField] private NewGameConfirmManager newGameConfirmManager;
 
+    [Header("Floor Testing")]
+    [SerializeField] private string startFloor = "Floor_01";
+
     [Header("Controls Menu Buttons")]
     [SerializeField] private Button cancelControlsButton;
     [SerializeField] private Button confirmControlsButton;
@@ -436,7 +439,7 @@ public class MenuManager : MonoBehaviour
     {
         if (LevelLoader.Instance == null || !LevelLoader.Instance.HasSavedGame)
         {
-            OnBeginGame("Floor_01");
+            OnBeginGame(startFloor);
             return;
         }
 
