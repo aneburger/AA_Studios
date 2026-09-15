71de91e7eed6ffb54a39ae650622adbe6d55dbcc
Anica-Ferreira <anicaferr@gmail.com>
2026-09-15 22:54:36 +0200

    Floor 08 refurnish

--- MarshRooms!/Assets/Scripts/UI/NewGameConfirmManager.cs
+++ MarshRooms!/Assets/Scripts/UI/NewGameConfirmManager.cs
@@ -190,7 +190,7 @@ public class NewGameConfirmManager : MonoBehaviour
         MenuManager menuManager = FindFirstObjectByType<MenuManager>();
         if (menuManager != null)
         {
-            menuManager.OnBeginGame("Floor_01");
+            menuManager.OnBeginGame("Floor_08");
         }
     }
 
